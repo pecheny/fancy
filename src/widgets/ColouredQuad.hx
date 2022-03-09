89cd01cf0563048936d56fd29f8c295300c1d7cb
artem pecheny <pecheny@gmail.com>
2022-03-10 00:02:24 +0300

    Fix ColorQuad.

--- src/widgets/ColouredQuad.hx
+++ src/widgets/ColouredQuad.hx
@@ -1,26 +1,33 @@
 package widgets;
-import data.aliases.AttribAliases;
 import al.al2d.Axis2D;
 import al.al2d.Widget2D;
 import crosstarget.Widgetable;
+import data.aliases.AttribAliases;
+import ec.CtxBinder;
+import gl.ec.DrawcallDataProvider;
+import gl.ec.Drawcalls;
 import gl.Renderable;
+import gl.RenderDataTarget;
 import gl.RenderTargets;
 import gl.sets.ColorSet;
 import gl.ValueWriter.AttributeWriters;
 import graphics.shapes.QuadGraphicElement;
 import haxe.io.Bytes;
+import mesh.MeshUtilss;
 import mesh.providers.AttrProviders.SolidColorProvider;
 import transform.AspectRatioProvider;
+import transform.LiquidTransformer;
 
-class ColouredQuad extends Widgetable implements Renderable<ColorSet>{
+class ColouredQuad extends Widgetable implements Renderable<ColorSet> {
     public var q:QuadGraphicElement<ColorSet>;
     var color:Int;
     var buffer:Bytes;
     var posWriter:AttributeWriters;
+
     public function new(w:Widget2D, color) {
         this.color = color;
         super(w);
-        buffer =  Bytes.alloc(4*ColorSet.instance.stride);
+        buffer = Bytes.alloc(4 * ColorSet.instance.stride);
         posWriter = ColorSet.instance.getWriter(AttribAliases.NAME_POSITION);
     }
 
@@ -28,25 +35,17 @@ class ColouredQuad extends Widgetable implements Renderable<ColorSet>{
 
     override function init() {
         var aspectRatio = ratioProvider.getFactorsRef();
-        var fluidTransform = new LiquidTransformator(aspectRatio);
+        var fluidTransform = new LiquidTransformer(aspectRatio);
         var e = w.entity;
         var renderTarget = new RenderDataTarget();
         q = new QuadGraphicElement(ColorSet.instance, fluidTransform.transformValue);
-
-//        q = fluidTransform.addChild(qq);
-//        colorContainer.build();
-
         for (a in Axis2D.keys) {
-            var applier2:GTransformAxisApplier = cast fluidTransform.getAxisApplier(a);
-            w.axisStates[a].addSibling(applier2);
+            w.axisStates[a].addSibling(fluidTransform.getAxisApplier(a));
         }
+        var drawcallsData = DrawcallDataProvider.get(ColorSet.instance, w.entity);
+        drawcallsData.views.push(this);
+        new CtxBinder(Drawcalls, w.entity);
         cp = new SolidColorProvider(0, 0, 0);
-//        new CtxBinder(Drawcalls, w.entity);
-//        setColor(Std.int(0x90a030 * Math.random()));
-//        var dp = new DrawcallDataProvider(ColorSet.instance);
-//        dp.views.push(new VertDataRenderer(ColorSet.instance, new SimpleBlitRenderer(ColorSet.instance, renderTarget.getBytes()), new SimpleIndexProvider(IndexCollection.forQuadsOdd(1))));
-//        w.entity.addComponentByType(DrawcallDataProvider, dp);
-        trace("done");
         setColor(color);
     }
 
