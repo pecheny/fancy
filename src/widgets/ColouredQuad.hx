bf2c75b9c753d7562edda8e736e701a7090c3c3c
artem pecheny <pecheny@gmail.com>
2022-03-21 16:08:20 +0300

    ColouredQuad inherits from ShapeWidget.

--- src/widgets/ColouredQuad.hx
+++ src/widgets/ColouredQuad.hx
@@ -1,67 +1,39 @@
 package widgets;
-import al.al2d.Axis2D;
 import al.al2d.Widget2D;
-import crosstarget.Widgetable;
 import data.aliases.AttribAliases;
-import ec.CtxBinder;
-import gl.ec.DrawcallDataProvider;
-import gl.ec.Drawcalls;
-import gl.Renderable;
-import gl.RenderDataTarget;
-import gl.RenderTargets;
 import gl.sets.ColorSet;
-import gl.ValueWriter.AttributeWriters;
 import graphics.shapes.QuadGraphicElement;
+import graphics.ShapeWidget;
 import haxe.io.Bytes;
 import mesh.MeshUtilss;
 import mesh.providers.AttrProviders.SolidColorProvider;
-import transform.AspectRatioProvider;
-import transform.LiquidTransformer;
 
-class ColouredQuad extends Widgetable implements Renderable<ColorSet> {
+class ColouredQuad extends ShapeWidget<ColorSet> {
     public var q:QuadGraphicElement<ColorSet>;
     var color:Int;
-    var buffer:Bytes;
-    var posWriter:AttributeWriters;
+    var cp:SolidColorProvider;
 
     public function new(w:Widget2D, color) {
         this.color = color;
-        super(w);
+        super(ColorSet.instance, w);
         buffer = Bytes.alloc(4 * ColorSet.instance.stride);
         posWriter = ColorSet.instance.getWriter(AttribAliases.NAME_POSITION);
     }
 
-    @:once var ratioProvider:AspectRatioProvider;
-
-    override function init() {
+    override function createShapes() {
         var aspectRatio = ratioProvider.getFactorsRef();
-        var fluidTransform = new LiquidTransformer(aspectRatio);
-        var e = w.entity;
-        var renderTarget = new RenderDataTarget();
-        q = new QuadGraphicElement(ColorSet.instance, fluidTransform.transformValue);
-        for (a in Axis2D.keys) {
-            w.axisStates[a].addSibling(fluidTransform.getAxisApplier(a));
-        }
-        var drawcallsData = DrawcallDataProvider.get(ColorSet.instance, w.entity);
-        drawcallsData.views.push(this);
-        new CtxBinder(Drawcalls, w.entity);
-        cp = new SolidColorProvider(0, 0, 0);
+        cp = SolidColorProvider.fromInt(color, 128);
+        var q = new QuadGraphicElement(ColorSet.instance, fluidTransform.transformValue);
+        children.push(q);
+    }
+
+    override function onShapesDone() {
         setColor(color);
     }
 
-    var cp:SolidColorProvider;
 
     public function setColor(c:Int) {
         cp.setColor(c);
-        MeshUtilss.writeInt8Attribute(ColorSet.instance, buffer, AttribAliases.NAME_COLOR_IN, 0, 4, cp.getValue);
+        MeshUtilss.writeInt8Attribute(attrs, buffer, AttribAliases.NAME_COLOR_IN, 0, vertsCount, cp.getValue);
     }
-
-    public function render(targets:RenderTargets<ColorSet>):Void {
-        var inds = q.getIndices();
-        targets.blitIndices(inds, inds.length);
-        q.writePostions(buffer, posWriter);
-        targets.blitVerts(buffer, 4);
-    }
-
-
 }
