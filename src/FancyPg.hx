2f8ee3dba05c11b4f9ae4bb8919de9b383f9af47
artem pecheny <pecheny@gmail.com>
2023-04-20 17:22:31 +0300

    More structure cleanup of FancyPg.

--- src/FancyPg.hx
+++ src/FancyPg.hx
@@ -35,6 +35,81 @@ using al.Builder;
 
 class FancyPg extends Sprite {
     var fuiBuilder = new FuiBuilder();
+    var sampleText = "FoEo Bar AbAb Aboo Distance Field texture Ad Ae Af Bd Be Bf Bb Ab Dd De Df Cd Ce Cf";
+    var b:PlaceholderBuilderGl;
+    var ar:AspectRatioProvider;
+    var dl = '<container>
+        <drawcall type="color"/>
+        <drawcall type="text" font=""/>
+        </container>';
+
+    public function new() {
+        super();
+        var rw = prepareRootContainer();
+
+        var scrollPlaceholder = createScrollbox(fuiBuilder.makeClickInput(b.b("c1").createContainer(vertical, Forward).widget()), b.b(), ar, dl,
+            createMixedContentArray);
+        rw.addWidget(scrollPlaceholder);
+
+        var cright = Builder.v().withChildren([
+            new Label(b.b(), sty("pcr")).withText(sampleText).widget(),
+            texturedQuad(fuiBuilder, b.h(pfr, 1).v(sfr, 0.5).b().withLiquidTransform(ar.getAspectRatio()), "bunie.png").widget(),
+        ]);
+        rw.addWidget(cright);
+    }
+
+    function prepareRootContainer() {
+        var root:Entity = new Entity();
+        fuiBuilder.regDefaultDrawcalls();
+        ar = fuiBuilder.ar;
+        b = new PlaceholderBuilderGl(fuiBuilder.ar);
+        createTextStyles();
+
+        root.addComponentByType(AspectRatioProvider, fuiBuilder.ar);
+        root.addComponentByType(WindowSizeProvider, fuiBuilder.ar);
+        root.addComponentByType(Stage, fuiBuilder.ar);
+        fuiBuilder.configureInput(root);
+        fuiBuilder.createContainer(root, Xml.parse(dl).firstElement());
+        var container:Sprite = root.getComponent(Sprite);
+        addChild(container);
+        var rw = Builder.h();
+        root.addChild(rw.entity);
+        new StageAspectResizer(rw.widget(), 2);
+        return rw;
+    }
+
+    function createScrollbox(container1:Placeholder2D, placeholder:Placeholder2D, ar, dl, childrenFactory:FuiBuilder->Array<Placeholder2D>) {
+        var cont = container1.entity.getComponent(Widget2DContainer);
+        var scroll = new W2CScrollableContent(cont, placeholder);
+
+        placeholder.entity.name = "placeholder";
+        var scroller = new ScrollboxItem(placeholder, scroll, ar.getAspectRatio());
+        fuiBuilder.addScissors(scroller.widget());
+        fuiBuilder.createContainer(scroller.widget().entity, Xml.parse(dl).firstElement());
+        for (ch in childrenFactory(fuiBuilder))
+            cont.addWidget(ch);
+        var spr:Sprite = scroller.widget().entity.getComponent(Sprite);
+        addChild(spr);
+        fuiBuilder.setAspects([]);
+        return placeholder;
+    }
+
+    public function texturedQuad(fuiBuilder:FuiBuilder, w:Placeholder2D, filename, createGldo = true):ShapeWidget<TexSet> {
+        var attrs = TexSet.instance;
+        var shw = new ShapeWidget(attrs, w);
+        shw.addChild(new QuadGraphicElement(attrs));
+        var uvs = new graphics.DynamicAttributeAssigner(attrs, shw.getBuffer());
+        uvs.fillBuffer = (attrs:TexSet, buffer) -> {
+            var writer = attrs.getWriter(AttribAliases.NAME_UV_0);
+            QuadGraphicElement.writeQuadPostions(buffer.getBuffer(), writer, 0, (a, wg) -> wg);
+        };
+        if (createGldo) {
+            fuiBuilder.createContainer(w.entity, Xml.parse('<container><drawcall type="image" path="Assets/$filename" /></container>').firstElement());
+            var spr:Sprite = w.entity.getComponent(Sprite);
+            addChild(spr);
+        }
+        return shw;
+    }
 
     function createTextStyles() {
         fuiBuilder.addBmFont("", "Assets/heaps-fonts/robo.fnt"); // todo
@@ -64,114 +139,41 @@ class FancyPg extends Sprite {
             .build();
     }
 
+    function createBarWidget() {
+        var elements = () -> [
+            new BarContainer(FixedThikness(new BarAxisSlot({pos: .5, thikness: 1.}, null)), Portion(new BarAxisSlot({start: 0., end: 1.}, null))),
+            new BarContainer(FixedThikness(new BarAxisSlot({pos: 0., thikness: 1.}, null)), Portion(new BarAxisSlot({start: 0., end: 1.}, null))),
+        ];
+
+        var attrs = ColorSet.instance;
+        var cq = new BarWidget(attrs, b.h(sfr, 1).v(sfr, 0.5).b("bars").withLiquidTransform(ar.getAspectRatio()), elements());
+        var colors = new ShapesColorAssigner(attrs, 0, cq.getBuffer());
+        return cq;
+    }
+
+    function createMixedContentArray(fuiBuilder) {
+        return [
+            new Label(b.h(sfr, 1).v(sfr, 0.4).b(), sty("pcl")).withText(sampleText).widget(),
+            new Label(b.h(sfr, 1).v(sfr, 0.4).b(), sty("pcc")).withText(sampleText).widget(),
+            new Label(b.h(sfr, 1).v(sfr, 0.4).b(), sty("pcr")).withText(sampleText).widget(),
+            new widgets.Slider(b.v(sfr, 0.1).b("slider r").withLiquidTransform(ar.getAspectRatio()), horizontal,
+                f -> trace("" + f)).withProgress(0.5).widget(),
+            texturedQuad(fuiBuilder, b.h(sfr, 1).v(sfr, 0.5).b().withLiquidTransform(ar.getAspectRatio()), "bunie.png").widget(),
+            new Button(b.h(sfr, 1).v(px, 60).b().withLiquidTransform(ar.getAspectRatio()), null, "Button caption", sty("fit")).widget(),
+            new Button(b.h(sfr, 1).v(sfr, 0.2).b().withLiquidTransform(ar.getAspectRatio()), null, "Button", sty("fit")).widget(),
+            createBarWidget().widget(),
+        ];
+    }
+
+    function lineHeightSample() {
+        // todo
+        return new Button(b.h(sfr, 1).v(sfr, 0.5).b().withLiquidTransform(ar.getAspectRatio()), null, "<font lineHeight=\"0.1\">Button </font>",
+            sty("fit")).widget();
+    }
+
     function sty(name) {
         // fuiBuilder.textStyles.
         return fuiBuilder.textStyles.getStyle(name);
-    }
-
-    public function new() {
-        super();
-        var sampleText = "FoEo Bar AbAb Aboo Distance Field texture Ad Ae Af Bd Be Bf Bb Ab Dd De Df Cd Ce Cf";
-        var root:Entity = new Entity();
-        fuiBuilder.regDefaultDrawcalls();
-        var ar = fuiBuilder.ar;
-        var b = new PlaceholderBuilderGl(ar);
-        //        fuiBuilder.addBmFont("", "Assets/heaps-fonts/monts.fnt"); // todo
-        createTextStyles();
-        root.addComponentByType(AspectRatioProvider, fuiBuilder.ar);
-        root.addComponentByType(WindowSizeProvider, fuiBuilder.ar);
-        root.addComponentByType(Stage, fuiBuilder.ar);
-        fuiBuilder.configureInput(root);
-
-        var dl = '<container>
-        <drawcall type="color"/>
-        <drawcall type="text" font=""/>
-        </container>';
-        fuiBuilder.createContainer(root, Xml.parse(dl).firstElement());
-
-        var container:Sprite = root.getComponent(Sprite);
-        addChild(container);
-
-        function cqFac() {
-            var elements = () -> [
-                new BarContainer(FixedThikness(new BarAxisSlot({pos: .5, thikness: 1.}, null)), Portion(new BarAxisSlot({start: 0., end: 1.}, null))),
-                new BarContainer(FixedThikness(new BarAxisSlot({pos: 0., thikness: 1.}, null)), Portion(new BarAxisSlot({start: 0., end: 1.}, null))),
-            ];
-
-            var attrs = ColorSet.instance;
-            var cq = new BarWidget(attrs, b.h(sfr, 1).v(sfr, 0.5).b("bars").withLiquidTransform(ar.getAspectRatio()), elements());
-            var colors = new ShapesColorAssigner(attrs, 0, cq.getBuffer());
-            return cq;
-        }
-
-        var pxW = b.b();
-        @:privateAccess pxW.axisStates[vertical].size = new PixelSize(vertical, ar, 60);
-        function createContent(fuiBuilder) {
-            return [
-                cqFac().widget(),
-                // new Label(b.b(), sty("pcc")).withText(sampleText).widget(),
-                // new Label(b.b(), sty("pcr")).withText(sampleText).widget(),
-                // new Button(b.h(sfr, 1).v(sfr, 0.5).b().withLiquidTransform(ar.getAspectRatio()), null, "<font lineHeight=\"0.1\">Button </font>",
-                //     sty("fit")).widget(),
-                texturedQuad(fuiBuilder, b.h(sfr, 1).v(sfr, 0.5).b().withLiquidTransform(ar.getAspectRatio()), "bunie.png").widget(),
-                new Button(pxW.withLiquidTransform(ar.getAspectRatio()), null, "Button caption", sty("fit")).widget(),
-                new Label(b.b(), sty("pcl")).withText(sampleText).widget(),
-                new Button(b.h(sfr, 1).v(sfr, 0.5).b().withLiquidTransform(ar.getAspectRatio()), null, "Button", sty("fit")).widget(),
-                new Button(b.h(sfr, 1).v(sfr, 0.5).b().withLiquidTransform(ar.getAspectRatio()), null, "Button", sty("fit")).widget(),
-                new Button(b.h(sfr, 1).v(sfr, 0.5).b().withLiquidTransform(ar.getAspectRatio()), null, "Button", sty("fit")).widget(),
-            ];
-        }
-        //        quads.push(new ColouredQuad(b.b().withLiquidTransform(ar.getAspectRatio()), 0x303090).widget());
-
-        var container1 = Builder.v().widget();
-        fuiBuilder.makeClickInput(container1);
-        container1.entity.name = "c1";
-        var placeholder = b.b();
-        createScrollbox(fuiBuilder, container1, placeholder, ar, dl, createContent);
-
-        var cright = Builder.v().withChildren([
-            new Label(b.b(), sty("pcr")).withText(sampleText).widget(),
-            new widgets.Slider(b.v(sfr, 0.1).b("slider r").withLiquidTransform(ar.getAspectRatio()), horizontal,
-                f -> trace("" + f)).withProgress(0.5).widget(),
-            texturedQuad(fuiBuilder, b.h(pfr, 1).v(sfr, 0.5).b().withLiquidTransform(ar.getAspectRatio()), "bunie.png").widget(),
-        ]);
-        var rw = Builder.h().withChildren([placeholder, cright]);
-        root.addChild(rw.entity);
-        new StageAspectResizer(rw, 2);
-        new DummyEditorField();
-    }
-
-    function createScrollbox(fuiBuilder:FuiBuilder, container1:Placeholder2D, placeholder:Placeholder2D, ar, dl,
-            childrenFactory:FuiBuilder->Array<Placeholder2D>) {
-        var cont = container1.entity.getComponent(Widget2DContainer);
-        var scroll = new W2CScrollableContent(cont, placeholder);
-
-        placeholder.entity.name = "placeholder";
-        var scroller = new ScrollboxItem(placeholder, scroll, ar.getAspectRatio());
-        fuiBuilder.addScissors(scroller.widget());
-        fuiBuilder.createContainer(scroller.widget().entity, Xml.parse(dl).firstElement());
-        for (ch in childrenFactory(fuiBuilder))
-            cont.addWidget(ch);
-        var spr:Sprite = scroller.widget().entity.getComponent(Sprite);
-        addChild(spr);
-        fuiBuilder.setAspects([]);
-    }
-
-    public function texturedQuad(fuiBuilder:FuiBuilder, w:Placeholder2D, filename, createGldo = true):ShapeWidget<TexSet> {
-        var attrs = TexSet.instance;
-        var shw = new ShapeWidget(attrs, w);
-        shw.addChild(new QuadGraphicElement(attrs));
-        var uvs = new graphics.DynamicAttributeAssigner(attrs, shw.getBuffer());
-        uvs.fillBuffer = (attrs:TexSet, buffer) -> {
-            var writer = attrs.getWriter(AttribAliases.NAME_UV_0);
-            QuadGraphicElement.writeQuadPostions(buffer.getBuffer(), writer, 0, (a, wg) -> wg);
-        };
-        if (createGldo) {
-            fuiBuilder.createContainer(w.entity, Xml.parse('<container><drawcall type="image" path="Assets/$filename" /></container>').firstElement());
-            var spr:Sprite = w.entity.getComponent(Sprite);
-            addChild(spr);
-        }
-        return shw;
     }
 
     function getSampleText() {
