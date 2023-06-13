3a92e964903a57ca4ff1e7227bd3558340bdb8e0
artem pecheny <pecheny@gmail.com>
2023-06-13 19:21:01 +0300

    SpriteAspectKeeper

---
+++ src/openfl/SpriteAspectKeeper.hx
@@ -0,0 +1,114 @@
+package openfl;
+
+import Axis2D;
+import a2d.AspectRatioProvider;
+import a2d.Boundbox;
+import a2d.Stage;
+import a2d.WindowSizeProvider;
+import al.al2d.Placeholder2D;
+import al.al2d.Widget2DContainer;
+import al.core.AxisApplier;
+import al.core.AxisApplier;
+import al.openfl.StageAspectResizer;
+import al.view.AspectKeeper;
+import algl.Builder.PlaceholderBuilderGl;
+import data.aliases.AttribAliases;
+import ec.Entity;
+import gl.sets.ColorSet;
+import gl.sets.TexSet;
+import graphics.ShapesColorAssigner;
+import graphics.shapes.Bar;
+import graphics.shapes.QuadGraphicElement;
+import macros.AVConstructor;
+import openfl.Lib;
+import openfl.SpriteAspectKeeper;
+import openfl.display.Sprite;
+import scroll.ScrollableContent.W2CScrollableContent;
+import scroll.ScrollboxItem;
+import widgets.BarWidget;
+import widgets.Button;
+import widgets.Label;
+import widgets.ShapeWidget;
+import widgets.Widget;
+import widgets.Widget;
+
+class SpriteAspectKeeper extends Widget {
+    var spr:Sprite;
+    var bounds:a2d.Boundbox;
+    var size = AVConstructor.create(Axis2D, 1., 1.);
+    var pos = AVConstructor.create(Axis2D, 0., 0.);
+    var ownSizeAppliers:AVector2D<AxisApplier>;
+    @:once var s:Stage;
+
+    public function new(w:Placeholder2D, spr:Sprite, bounds = null) {
+        super(w);
+        this.spr = spr;
+        this.bounds = if (bounds == null) {
+            var b = spr.getBounds(spr);
+            new Boundbox(b.left, b.top, b.width, b.height);
+        } else bounds;
+
+        for (a in Axis2D) {
+            w.axisStates[a].addSibling(new KeeperAxisApplier(pos, size, this, a));
+        }
+    }
+
+    public function refresh() {
+        if (!_inited)
+            return;
+        var scale = 9999.;
+        for (a in Axis2D) {
+            var _scale = size[a] / bounds.size[a];
+            if (_scale < scale)
+                scale = _scale;
+        }
+
+        for (a in Axis2D) {
+            var free = size[a] - bounds.size[a] * scale;
+            var pos = pos[a] + free / 2;
+            apply(a, pos, scale);
+        }
+    }
+
+    inline function apply(a:Axis2D, pos:Float, scale:Float) {
+        switch a {
+            case horizontal:
+                spr.x = w2scr(a, pos);
+                spr.scaleX = w2scr(a, scale);
+            case vertical:
+                spr.y = w2scr(a, pos);
+                spr.scaleY = w2scr(a, scale);
+        }
+        // trace(this.pos[a]  + " " + size[a]);
+        // trace(spr.x  +" " + spr.scaleX  + " " + spr.width);
+    }
+
+    inline function w2scr(a, val:Float) {
+        return val * s.getWindowSize()[a] / s.getAspectRatio()[a] / 2;
+    }
+
+    public function getApplier(a:Axis2D) {
+        return ownSizeAppliers[a];
+    }
+}
+
+class KeeperAxisApplier implements AxisApplier {
+    var key:Axis2D;
+    var keeper:SpriteAspectKeeper;
+
+    var size:AVector2D<Float>;
+    var pos:AVector2D<Float>;
+
+    public function new(p, s, k, a) {
+        this.pos = p;
+        this.size = s;
+        this.keeper = k;
+        this.key = a;
+    }
+
+    public function apply(pos:Float, size:Float):Void {
+        this.pos[key] = pos;
+        this.size[key] = size;
+        keeper.refresh();
+    }
+}
