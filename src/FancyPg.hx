8f4f99d8876e269d4c57c054ea2cb32761d35f13
artem pecheny <pecheny@gmail.com>
2023-06-15 19:00:57 +0300

    Minor amend for SpriteAspectKeeper extraction.

--- src/FancyPg.hx
+++ src/FancyPg.hx
@@ -1,5 +1,6 @@
 package;
 
+import openfl.SpriteAspectKeeper;
 import a2d.Boundbox;
 import macros.AVConstructor;
 import al.core.AxisApplier;
@@ -188,89 +189,7 @@ class FancyPg extends Sprite {
         spr.graphics.drawRect(0, 0, 30, 30);
         spr.graphics.endFill();
         Lib.current.addChild(spr);
-        return new AspectKeeper(w, spr);
+        return new SpriteAspectKeeper(w, spr);
         // return new SpriteAdapter(w, spr);
-    }
-}
-
-class AspectKeeper extends Widget {
-    var spr:Sprite;
-    var bounds:a2d.Boundbox;
-    var size = AVConstructor.create(Axis2D, 1., 1.);
-    var pos = AVConstructor.create(Axis2D, 0., 0.);
-    var ownSizeAppliers:AVector2D<AxisApplier>;
-    @:once var s:Stage;
-
-    public function new(w:Placeholder2D, spr:Sprite, bounds = null) {
-        super(w);
-        this.spr = spr;
-        this.bounds = if (bounds == null) {
-            var b = spr.getBounds(spr);
-            new Boundbox(b.left, b.top, b.width, b.height);
-        } else bounds;
-
-        for (a in Axis2D) {
-            w.axisStates[a].addSibling(new KeeperAxisApplier(pos, size, this, a));
-        }
-    }
-
-    public function refresh() {
-        if (!_inited)
-            return;
-        var scale = 9999.;
-        for (a in Axis2D) {
-            var _scale = size[a] / bounds.size[a];
-            if (_scale < scale)
-                scale = _scale;
-        }
-
-        for (a in Axis2D) {
-            var free = size[a] - bounds.size[a] * scale;
-            var pos = pos[a] + free / 2;
-            trace(a, pos, scale);
-            apply(a, pos, scale);
-        }
-    }
-
-    inline function apply(a:Axis2D, pos:Float, scale:Float) {
-        switch a {
-            case horizontal:
-                spr.x = w2scr(a, pos);
-                spr.scaleX = w2scr(a, scale);
-            case vertical:
-                spr.y = w2scr(a, pos);
-                spr.scaleY = w2scr(a, scale);
-        }
-        // trace(this.pos[a]  + " " + size[a]);
-        // trace(spr.x  +" " + spr.scaleX  + " " + spr.width);
-    }
-
-    inline function w2scr(a, val:Float) {
-        return val * s.getWindowSize()[a] / s.getAspectRatio()[a] / 2;
-    }
-
-    public function getApplier(a:Axis2D) {
-        return ownSizeAppliers[a];
-    }
-}
-
-class KeeperAxisApplier implements AxisApplier {
-    var key:Axis2D;
-    var keeper:AspectKeeper;
-
-    var size:AVector2D<Float>;
-    var pos:AVector2D<Float>;
-
-    public function new(p, s, k, a) {
-        this.pos = p;
-        this.size = s;
-        this.keeper = k;
-        this.key = a;
-    }
-
-    public function apply(pos:Float, size:Float):Void {
-        this.pos[key] = pos;
-        this.size[key] = size;
-        keeper.refresh();
     }
 }
