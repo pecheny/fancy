f20795fcab659b8bb86a34e3e6611b8680586688
artem pecheny <pecheny@gmail.com>
2024-06-06 14:54:12 +0300

    GameplayUIMock minor api changes.

--- src/ui/GameplayUIMock.hx
+++ src/ui/GameplayUIMock.hx
@@ -17,12 +17,18 @@ interface GameMock {
 class GameplayPauseScreen extends Widget {
     var pnl:Widget2DContainer;
     var game:GameMock;
+    @:once var fb:FuiBuilder;
 
-    public function new(w:Placeholder2D, root:Entity, g) {
+    public function new(w:Placeholder2D,  g) {
         super(w);
         this.game = g;
-        pnl = Builder.createContainer(w, vertical, Forward);
-        var fb = root.getComponentUpward(FuiBuilder);
+    }
+
+    override function init() {
+        super.init();
+        pnl = Builder.createContainer(ph, vertical, Forward);
+        fb.makeClickInput(ph);
+        // var fb = root.getComponentUpward(FuiBuilder);
         var b = fb.placeholderBuilder;
         pnl.withChildren([
             b.h(pfr, 1).v(pfr, 1).b(),
@@ -35,18 +41,23 @@ class GameplayPauseScreen extends Widget {
 class GameplayScreen extends Widget {
     var pnl:Widget2DContainer;
     var game:GameMock;
+    @:once var fb:FuiBuilder;
 
-    public function new(w:Placeholder2D, root:Entity, g) {
+    public function new(w:Placeholder2D, g) {
         super(w);
         this.game = g;
-        pnl = Builder.createContainer(w, vertical, Forward);
-        var fb = root.getComponentUpward(FuiBuilder);
+    }
+
+    override function init() {
+        pnl = Builder.createContainer(ph, vertical, Forward);
+        // var fb = root.getComponentUpward(FuiBuilder);
         var b = fb.placeholderBuilder;
         pnl.withChildren([topBar(b.v(sfr, 0.05).b("top"), fb)]);
     }
 
     function topBar(w:Placeholder2D, fb:FuiBuilder) {
         var b = fb.placeholderBuilder;
+        fb.makeClickInput(w);
         w.createContainer(horizontal, Backward).withChildren([
             new Button(b.h(sfr, .1).v(pfr, 1).b().withLiquidTransform(fb.ar.getAspectRatio()), () -> game.pause(true), "||", fb.s("fit")).ph,
         ]);
