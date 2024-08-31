b0b42fa79780771e2e1b0ea59b1dc94f4e40e8fa
artem pecheny <pecheny@gmail.com>
2024-08-31 12:10:18 +0300

    Extract RenderingPipeline to flgl.

--- src/FuiBuilder.hx
+++ src/FuiBuilder.hx
@@ -1,5 +1,6 @@
 package;
 
+import gl.RenderingPipeline;
 import gl.OflGLNodeAdapter;
 import openfl.Lib;
 import gl.GLNode;
@@ -65,98 +66,6 @@ class XmlLayerLayouts {
     </container>';
 }
 
-class RenderingPipeline {
-	public var renderAspectBuilder(default, null):RenderAspectBuilder;
-	public var textureStorage:TextureStorage;
-	public var shaderRegistry:ShaderRegistry;
-
-	var pos:ShaderElement = PosPassthrough.instance;
-
-	public function new() {
-		renderAspectBuilder = new RenderAspectBuilder();
-		textureStorage = new TextureStorage();
-		shaderRegistry = new ShaderRegistry();
-	}
-
-	public function hasDrawcallType(type) {
-		return (shaderRegistry.getDescr(type) != null);
-	}
-
-	public function addAspect(a:RenderingAspect) {
-		renderAspectBuilder.addShared(a);
-	}
-
-	public function setPositioning(pos:ShaderElement) {
-		this.pos = pos;
-		return this;
-	}
-
-	public function createContainer(e:Entity, descr):Entity {
-		RenderableBinder.getOrCreate(e); // to prevent
-		var node = processNode(e, descr);
-		renderAspectBuilder.reset();
-		bindLayer(e, node);
-		var adapter = new OflGLNodeAdapter();
-		adapter.addNode(node);
-		Lib.current.stage.addChild(adapter);
-		return e;
-	}
-
-	function bindLayer(e, glnode:GLNode) {
-		var binder = RenderableBinder.getOrCreate(e);
-		if (Std.isOfType(glnode, ContainerGLNode)) {
-			var c = cast(glnode, ContainerGLNode);
-			for (ch in c.children)
-				bindLayer(e, ch);
-		}
-		if (Std.isOfType(glnode, ShadedGLNode)) {
-			var gldo:ShadedGLNode<AttribSet> = cast glnode;
-			binder.bindLayer(e, gldo.set, glnode.name, gldo);
-		}
-	}
-
-	var handlers:Map<String, PassBase<Dynamic>> = new Map();
-
-	public function processNode(e:Entity, node:Xml, ?container:Null<ContainerGLNode>):GLNode {
-		return switch (node.nodeName) {
-			case "container": {
-					var c = new ContainerGLNode();
-					if (container != null)
-						container.addChild(c);
-					for (child in node.elements()) {
-						processNode(e, child, c);
-					}
-					c;
-				}
-			case "drawcall": {
-					var type = node.get("type");
-					if (!handlers.exists(type)) {
-						trace('No "$type" drawcall type was registered.');
-						return container;
-					}
-					var pass = handlers[type];
-					renderAspectBuilder.newChain();
-
-					if (pass.aspectRegistrator != null)
-						pass.aspectRegistrator(node, renderAspectBuilder);
-					var gldo = new ShadedGLNode(pass.attr, shaderRegistry.getState.bind(pass.attr, _, type), renderAspectBuilder.build());
-
-					if (pass.layerNameExtractor != null)
-						gldo.name = pass.layerNameExtractor(node);
-					if (container != null)
-						container.addChild(gldo);
-					gldo;
-				}
-			case _:
-				throw "wrong " + node.nodeName;
-		}
-	}
-
-	public function addPass<TAtt:AttribSet>(p:gl.passes.PassBase<TAtt>) {
-		shaderRegistry.reg(p.getShaderDesc());
-        handlers[p.drawcallType] = p;
-	}
-}
 
 class FuiBuilder {
 	public var pipeline:RenderingPipeline;
@@ -223,8 +132,28 @@ class FuiBuilder {
 	}
 
 	public function createContainer(e:Entity, descr):Entity {
-		return pipeline.createContainer(e, descr);
+        RenderableBinder.getOrCreate(e); // to prevent
+		var node = pipeline.createContainer(descr); 
+		bindLayer(e, node);
+		var adapter = new OflGLNodeAdapter();
+		adapter.addNode(node);
+		Lib.current.stage.addChild(adapter);
+		return e;
 	}
+
+    function bindLayer(e, glnode:GLNode) {
+		var binder = RenderableBinder.getOrCreate(e);
+		if (Std.isOfType(glnode, ContainerGLNode)) {
+			var c = cast(glnode, ContainerGLNode);
+			for (ch in c.children)
+				bindLayer(e, ch);
+		}
+		if (Std.isOfType(glnode, ShadedGLNode)) {
+			var gldo:ShadedGLNode<AttribSet> = cast glnode;
+			binder.bindLayer(e, gldo.set, glnode.name, gldo);
+		}
+	}
+
 
 	public function addBmFont(fontName, fntPath) {
 		var font = fonts.initFont(fontName, fntPath, null);
