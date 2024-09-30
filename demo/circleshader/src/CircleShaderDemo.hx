2bf1892092607e1587a82c3f821ece67a8bd3103
artem pecheny <pecheny@gmail.com>
2024-09-30 22:54:54 +0300

    Circle shader demo.

---
+++ demo/circleshader/src/CircleShaderDemo.hx
@@ -0,0 +1,58 @@
+package;
+
+import data.IndexCollection;
+import haxe.io.Bytes;
+import gl.passes.CirclePass;
+import FuiBuilder;
+import a2d.Placeholder2D;
+import al.ec.WidgetSwitcher;
+import data.aliases.AttribAliases;
+import ec.Entity;
+import fu.graphics.ShapeWidget;
+import gl.sets.CircleSet;
+import graphics.shapes.QuadGraphicElement;
+import graphics.shapes.Shape;
+import openfl.display.Sprite;
+
+using a2d.transform.LiquidTransformer;
+using al.Builder;
+
+class CircleShaderDemo extends Sprite {
+    public var fui:FuiBuilder;
+    public var switcher:WidgetSwitcher<Axis2D>;
+
+    var e:Entity;
+
+    public function new() {
+        super();
+        fui = new FuiBuilder();
+
+        var pipeline = fui.pipeline;
+        pipeline.addPass("circle", new CirclePass());
+
+        fui.regDefaultDrawcalls = () -> {};
+        e = fui.createDefaultRoot(Xml.parse('<drawcall type="circle"/>').firstElement());
+        switcher = e.getComponent(WidgetSwitcher);
+        var wdg = quad(fui.placeholderBuilder.h(sfr, 1).v(sfr, 1).b(), 0x6a00ff);
+        var c = Builder.h();
+        Builder.addWidget(c, wdg.ph);
+        switcher.switchTo(c.ph);
+    }
+
+    public function quad(ph:Placeholder2D, color) {
+        fui.lqtr(ph);
+        var attrs = CircleSet.instance;
+        var shw = new ShapeWidget(attrs, ph, true);
+        shw.addChild(new QuadGraphicElement(attrs));
+        var uvs = new graphics.DynamicAttributeAssigner(attrs, shw.getBuffer());
+        uvs.fillBuffer = (attrs:CircleSet, buffer) -> {
+            var writer = attrs.getWriter(AttribAliases.NAME_UV_0);
+            QuadGraphicElement.writeQuadPostions(buffer.getBuffer(), writer, 0, (a, wg) -> wg);
+            attrs.fillFloat(buffer.getBuffer(), CircleSet.R1_IN, 0.3, 0, 4);
+            attrs.fillFloat(buffer.getBuffer(), CircleSet.R2_IN, 0.9, 0, 4);
+        };
+
+        shw.manInit();
+        return shw;
+    }
+}
