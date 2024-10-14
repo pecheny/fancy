e9ab29dacdd247a2f908676360a20dd322af15c1
artem pecheny <pecheny@gmail.com>
2024-10-14 16:18:57 +0300

    Move animation stuff to alayout.

--- demo/animation/src/widgets/WonderButton.hx
+++ demo/animation/src/widgets/WonderButton.hx
@@ -1,11 +1,7 @@
 package widgets;
 
-import al.animation.Animation.AnimContainer;
-import al.animation.Animation.AnimationPlaceholder;
-import al.animation.AnimationTreeBuilder;
-import ec.Component;
-import ec.CtxWatcher.CtxBinder;
-import ec.CtxWatcher;
+import al.animation.Animation;
+import al.animation.AnimationTreeComponent;
 import ec.Entity;
 import fu.PropStorage;
 import fu.graphics.BarWidget;
@@ -66,82 +62,5 @@ class WonderKit {
         preset.mapping.push(AnimationSlotSelectors.pathSelector.bind([0, 1]));
         props.set(AnimationTreeComponent.getId(WonderButton), preset);
         e.addComponentByType(PropStorage, props);
-    }
-}
-
-typedef Selector = AnimationPlaceholder->AnimationPlaceholder;
-
-class AnimationPreset {
-    public var treeDesc(default, null):Dynamic; //  uikit+class based preset
-    public var mapping(default, null):Array<Selector> = []; // uikit+class based preset
-
-    public function new(descr) {
-        this.treeDesc = descr;
-    }
-}
-
-class AnimationSlotSelectors {
-    public static function pathSelector(path:Array<Int>, aph:AnimationPlaceholder) {
-        return aph.entity.getGrandchild(path).getComponent(AnimationPlaceholder);
-    }
-}
-
-interface Channels {
-    var channels(default, null):Array<Float->Void>;
-}
-
-class AnimationTreeBinder implements CtxBinder {
-    var container:AnimContainer;
-
-    public function new(container) {
-        this.container = container;
-    }
-
-    public function bind(e:Entity) {
-        var acomp = e.getComponent(AnimationTreeComponent);
-        if (acomp != null) {
-            AnimationTreeBuilder.addChild(container, acomp.tree);
-            container.refresh();
-        }
-    }
-
-    public function unbind(e:Entity) {
-        var acomp = e.getComponent(AnimationTreeComponent);
-        if (acomp != null) {
-            AnimationTreeBuilder.removeChild(container, acomp.tree);
-            container.refresh();
-        }
-    }
-}
-
-class AnimationTreeComponent extends Component {
-    public var tree(default, null):AnimationPlaceholder; // can be constructed at place and binded to parent w Ctx
-
-    var target:Channels;
-    var alias:String;
-    @:once var props:PropStorage<AnimationPreset>;
-    @:once var builder:AnimationTreeBuilder;
-
-    public function new(e, target, alias = "") {
-        this.target = target;
-        this.alias = alias;
-        super(e);
-    }
-
-    override function init() {
-        var preset = props.get(getId(target, alias));
-        tree = builder.build(preset.treeDesc);
-        for (i in 0...target.channels.length)
-            preset.mapping[i](tree).channels.push(target.channels[i]);
-        new CtxWatcher(AnimationTreeBinder, entity);
-    }
-
-    public function setTime(t):Void {
-        if (_inited)
-            tree.setTime(t);
-    }
-
-    public static function getId(instance:Dynamic, alias = "") {
-        return Entity.getComponentId(instance) + "_" + alias;
     }
 }
