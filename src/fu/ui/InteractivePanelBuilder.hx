ae57f3ea6e8cd206e99b36913577c4823344b681
artem pecheny <pecheny@gmail.com>
2025-01-06 02:45:27 +0300

    Minor panel builder cleanup/imps.

--- src/fu/ui/InteractivePanelBuilder.hx
+++ src/fu/ui/InteractivePanelBuilder.hx
@@ -9,19 +9,17 @@ import al.core.WidgetContainer.WContainer;
 import fu.Signal.IntSignal;
 import fu.ui.ButtonBase;
 
-
 /**
     You init container with array of TData instead of placeholders and the DataChildrenPool creates and init TButton for each TData.
     InteractivePanelBuilder is a factory for DataChildrenPool, which allows to construct the item representation step by step, defining view and input components separately.
     The input handler has access to id of the clicked item, so if it has acces to the same array of TData, it can react properly. 
 **/
 class InteractivePanelBuilder<TData, TButton:IWidget<Axis2D> & DataView<TData>> {
-    var onChoise:IntSignal;
+    var onChoice:IntSignal;
     var buttons:DataChildrenPool<TData, TButton>;
     var wc:WContainer<Placeholder<Axis2D>>;
     var widgetFactory:Void->TButton;
 
-    // var handler:Int->Void;
     public function new() {}
 
     function factory(n:Int):TButton {
@@ -31,7 +29,8 @@ class InteractivePanelBuilder<TData, TButton:IWidget<Axis2D> & DataView<TData>> 
     }
 
     dynamic function inputHandlerFactory(ph:Placeholder2D, n:Int) {
-        new ButtonBase(ph, onChoise.dispatch.bind(n));
+        if (onChoice != null)
+            new ButtonBase(ph, onChoice.dispatch.bind(n));
     }
 
     public function withInput(fac:(ph:Placeholder2D, n:Int) -> Void) {
@@ -50,14 +49,9 @@ class InteractivePanelBuilder<TData, TButton:IWidget<Axis2D> & DataView<TData>> 
     }
 
     public function withSignal(sig:IntSignal) {
-        onChoise = sig;
+        onChoice = sig;
         return this;
     }
-
-    // public function withHandler(h:Int->Void) {
-    //     handler = h;
-    //     return this;
-    // }
 
     public function build() {
         buttons = new DataChildrenPool(wc, factory);
