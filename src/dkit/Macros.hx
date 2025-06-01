ca38b456d7cecd325915b125feca315e94ab9bb8
artem pecheny <pecheny@gmail.com>
2025-06-01 18:38:15 +0300

    Workaround preventing multiple initDkit() calls.

--- src/dkit/Macros.hx
+++ src/dkit/Macros.hx
@@ -1,16 +1,26 @@
 package dkit;
 
+import fu.macros.FieldUtils;
 import haxe.macro.Expr.Field;
 import haxe.macro.Type.ClassType;
 import haxe.macro.Context;
 
 class DefaultConstructorBuilder {
     static var template = macro class Templ {
+        var _dkit_called = false;
+
+        function _initDkit() {
+            if(_dkit_called)
+                return;
+            _dkit_called = true;
+            initDkit();
+        }
+
         public function new(p:a2d.Placeholder2D, ?parent:dkit.Dkit.BaseDkit) {
             super(p, parent);
             initComponent();
             if (parent == null)
-                initDkit();
+                _initDkit();
         }
     };
 
@@ -22,15 +32,18 @@ class DefaultConstructorBuilder {
                 required = false;
                 switch f.kind {
                     case FFun({expr: {expr: EBlock(exprs)}}):
-                        exprs.push(macro if (parent == null) initDkit());
+                        exprs.push(macro if (parent == null) _initDkit());
                     case _:
                         throw "Incompatible constructor";
                 }
                 break;
             }
         }
-        if (required)
-            fields.push(template.fields[0]);
+        for (f in template.fields)
+            if (!FieldUtils.hasField(f.name))
+                fields.push(f);
+        // if (required)
+        //     fields = fields.concat(template.fields);
 
         return fields;
     }
