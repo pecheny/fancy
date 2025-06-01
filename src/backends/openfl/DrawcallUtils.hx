3d65301079c5b7022bc7ed4bbd4e822459548db8
artem pecheny <pecheny@gmail.com>
2025-06-01 18:30:39 +0300

    Rebindable flgl containers.

--- src/backends/openfl/DrawcallUtils.hx
+++ src/backends/openfl/DrawcallUtils.hx
@@ -1,38 +1,39 @@
-
 package backends.openfl;
 
+import al.openfl.display.DrawcallDataProvider;
+import al.openfl.display.FlashDisplayRoot;
+import ec.CtxWatcher;
 import ec.Entity;
+import ecbind.RenderableBinder;
 import gl.AttribSet;
 import gl.GLNode;
 import gl.OflGLNodeAdapter;
-import gl.aspects.AlphaBlendingAspect;
-import ecbind.RenderableBinder;
 import gl.RenderingPipeline;
+import gl.aspects.AlphaBlendingAspect;
 
 class DrawcallUtils {
-    
-	public static function createContainer(pipeline:RenderingPipeline,e:Entity, descr):Entity {
-		RenderableBinder.getOrCreate(e); // to prevent
-		var node = pipeline.createContainer(descr);
-		node.addAspect(new AlphaBlendingAspect());
-		bindLayer(e, node);
-		var adapter = new OflGLNodeAdapter();
-		adapter.addNode(node);
-        e.addComponent(adapter);
-		openfl.Lib.current.stage.addChild(adapter);
-		return e;
-	}
+    public static function createContainer(pipeline:RenderingPipeline, e:Entity, descr):Entity {
+        RenderableBinder.getOrCreate(e); // to prevent
+        var node = pipeline.createContainer(descr);
+        node.addAspect(new AlphaBlendingAspect());
+        bindLayer(e, node);
+        var adapter = new OflGLNodeAdapter();
+        adapter.addNode(node);
+        DrawcallDataProvider.get(e).addView(adapter);
+        new CtxWatcher(FlashDisplayRoot, e);
+        return e;
+    }
 
-	static function bindLayer(e, glnode:GLNode) {
-		var binder = RenderableBinder.getOrCreate(e);
-		if (Std.isOfType(glnode, ContainerGLNode)) {
-			var c = cast(glnode, ContainerGLNode);
-			for (ch in c.children)
-				bindLayer(e, ch);
-		}
-		if (Std.isOfType(glnode, ShadedGLNode)) {
-			var gldo:ShadedGLNode<AttribSet> = cast glnode;
-			binder.bindLayer(e, gldo.set, glnode.name, gldo);
-		}
-	}
+    static function bindLayer(e, glnode:GLNode) {
+        var binder = RenderableBinder.getOrCreate(e);
+        if (Std.isOfType(glnode, ContainerGLNode)) {
+            var c = cast(glnode, ContainerGLNode);
+            for (ch in c.children)
+                bindLayer(e, ch);
+        }
+        if (Std.isOfType(glnode, ShadedGLNode)) {
+            var gldo:ShadedGLNode<AttribSet> = cast glnode;
+            binder.bindLayer(e, gldo.set, glnode.name, gldo);
+        }
+    }
 }
