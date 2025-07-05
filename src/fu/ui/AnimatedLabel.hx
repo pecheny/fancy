555be7e3a73ec0b370e6651afeb793177f0d315b
artem pecheny <pecheny@gmail.com>
2025-07-05 14:09:32 +0300

    Fix animated label.

--- src/fu/ui/AnimatedLabel.hx
+++ src/fu/ui/AnimatedLabel.hx
@@ -1,27 +1,26 @@
 package fu.ui;
 
-import htext.AttributeFiller.AttFillContainer;
-import htext.TextColorFiller;
+import a2d.transform.TransformerBase;
 import al.animation.Animation.Animatable;
+import gl.AttribSet;
 import gl.sets.CMSDFSet;
+import haxe.io.Bytes;
+import htext.AttributeFiller;
 import htext.ITextRender;
 import htext.SmothnessWriter;
-import htext.TextRender;
+import htext.TextLayouter;
 import htext.animation.VUnfoldAnimTextRender;
-import a2d.transform.TransformerBase;
-
 
 using htext.TextTransformer;
 
 class AnimatedLabel extends LabelBase<CMSDFSet> implements Animatable {
 	public function new(w, tc) {
-		createTextRender = _createTextRender;
 		super(w, tc, CMSDFSet.instance);
 	}
 
 	var _render:VUnfoldAnimTextRender<CMSDFSet>;
 
-	function _createTextRender(attrs:CMSDFSet, l, tt:TransformerBase):ITextRender<CMSDFSet> {
+	override function createTextRender(attrs:CMSDFSet, l:TextLayouter, tt:TransformerBase):ITextRender<CMSDFSet> {
 		var dpiWriter = attrs.getWriter(CMSDFSet.NAME_DPI);
         var wrs = new AttFillContainer();
         wrs.addChild(new SmothnessWriter(dpiWriter[0], l, textStyleContext, tt, stage.getWindowSize()));
@@ -35,5 +34,23 @@ class AnimatedLabel extends LabelBase<CMSDFSet> implements Animatable {
 	public function setTime(t:Float):Void {
 		_render.setTime(t);
 	}
+}
+
+
+class TextColorFiller<T:AttribSet> implements AttributeFiller {
+    var attr:T;
+
+    public var color:Int = 0xffffff;
+
+    var layouter:TextLayouter;
+
+    public function new(attrs:T, l) {
+        this.attr = attrs;
+        this.layouter = l;
+    }
+
+    public function write(target:Bytes, start) {
+        attr.writeColor(target, color, start, 4 * layouter.getTiles().length);
+    }
 }
 
