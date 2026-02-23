55371bc7f4c1cd3b683a5be4bfa8f9655ce305f6
artem pecheny <pecheny@gmail.com>
2026-02-23 13:17:40 +0300

    Remove DynamicTextRender.

--- src/htext/animation/VUnfoldAnimTextRender.hx
+++ src/htext/animation/VUnfoldAnimTextRender.hx
@@ -1,32 +1,49 @@
 package htext.animation;
+import data.IndexCollection;
 import Axis2D;
 import font.GLGlyphData.TileRecord;
 import gl.AttribSet;
-class VUnfoldAnimTextRender<T:AttribSet> extends DynamicTextRender<T> {
+class VUnfoldAnimTextRender<T:AttribSet> extends TextRender<T> {
 //    var lastTime = 0.;
     var time = 0.;
     var conv = new SeqTimeConverter(10);
 
     public function setTime(t:Float):Void {
         time = t;
-        dirty = true;
+        dirty = full;
+    }
+    
+    override function fillBuffer() {
+        charsLayouter.setText(value);
+        var tiles = charsLayouter.getTiles();
+        efficientLen = tiles.length;
+        if (TextRender.indices == null || TextRender.indices.length < efficientLen * 6)
+            TextRender.indices = IndexCollection.forQuads(efficientLen);
+        bytes.grantCapacity(4 * efficientLen * attrs.stride);
+        positions.resize(tiles.length * 4 * 2);
+        for (i in 0...efficientLen)
+            _setChar(i, tiles[i]);
+        if (otherAttributesToFill != null) {
+            otherAttributesToFill.write(bytes.bytes, 0);
+        }
+        dirty = transform;
     }
 
-    override function setChar(at:Int, rec:TileRecord) {
+    function _setChar(at:Int, rec:TileRecord) {
         for (i in 0...4) {
-            setVert(rec, horizontal, i, at);
-            setVert(rec, vertical, i, at);
+            _setVert(rec, horizontal, i, at);
+            _setVert(rec, vertical, i, at);
         }
     }
 
-    inline function setVert(rec:TileRecord, a:Axis2D, vert:Int, at) {
+    function _setVert(rec:TileRecord, a:Axis2D, vert:Int, at) {
         var targ = bytes.bytes;
         var vertOfs = at * 4;
-        posWriter[a].setValue(targ, vertOfs + vert, getVertPos(rec, a, vert, at));
+        posWriter[a].setValue(targ, vertOfs + vert, getVertPosAt(rec, a, vert, at));
         uvWriter[a].setValue(targ, vertOfs + vert, rec.tile.getUV(vert, a));
     }
 
-    inline function getVertPos(rec:TileRecord, a:Axis2D, vert:Int, at:Int) {
+    function getVertPosAt(rec:TileRecord, a:Axis2D, vert:Int, at:Int) {
         var lt = conv.getLocalTime(at, time);
         if (lt == 0)
             return 0.;
