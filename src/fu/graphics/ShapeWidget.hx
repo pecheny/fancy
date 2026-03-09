fdb386e195bedb217c25195677c5e0da8404dd6f
artem pecheny <pecheny@gmail.com>
2026-03-09 19:02:05 +0300

    ShapeWidget: layer alias argument.

--- src/fu/graphics/ShapeWidget.hx
+++ src/fu/graphics/ShapeWidget.hx
@@ -26,13 +26,13 @@ class ShapeWidget<T:AttribSet> extends Widget implements Renderable<T> {
     // do not call initialization before manual call in order to construct shapes on placeholder with all deps.
     var delayInit = false;
 
-    public function new(attrs:T, w:Placeholder2D, delayInit = false) {
-        var drawcallsData = RenderablesComponent.get(attrs, w.entity);
+    public function new(attrs:T, w:Placeholder2D, delayInit = false, layerAlias = "") {
+        var drawcallsData = RenderablesComponent.get(attrs, w.entity, layerAlias);
         this.attrs = attrs;
         this.delayInit = delayInit;
         shapeRenderer = new ShapeRenderer(attrs);
         super(w);
-        var drawcallsData = RenderablesComponent.get(attrs, w.entity);
+        // var drawcallsData = RenderablesComponent.get(attrs, w.entity);
         drawcallsData.views.push(this);
         new CtxWatcher(RenderableBinder, w.entity);
     }
