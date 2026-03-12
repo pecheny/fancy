8acb32f799b016f14d7a13e3e03f32a8495e8ac7
artem pecheny <pecheny@gmail.com>
2026-03-12 13:16:02 +0300

    Depth.configureDepthRange().

--- src/fu/depth/Depth.hx
+++ src/fu/depth/Depth.hx
@@ -23,23 +23,29 @@ typedef DepthRangeContainer = WidgetContainer<DepthAxis, DepthPh>;
 class Depth {
     public static function configureContainer(e:Entity, zindex = 0) {
         al.prop.DepthComponent.getOrCreate(e);
+        var ph = configureDepthRange(e, zindex);
+        var c = new DepthRangeContainer(ph, 1);
+        c.setLayout(depth, PortionLayout.instance);
+        c.refreshOnChildrenChanged = true;
+        e.addComponent(c);
+        e.addComponent(new DepthBinder(c));
+
+        // ec.DebugInit.initCheck.listen((_:Entity) -> {
+        //     trace(e.getPath(), c.getChildren().map(ph -> ph.entity.name + " " + ph.axisStates[depth].getPos() + " " + ph.axisStates[depth].getSize()),
+        //         '[${dc.value}, ${dc.maxValue}]');
+        // });
+    }
+
+    public static function configureDepthRange(e:Entity, zindex = 0) {
         var dc = DepthRangeComponent.getOrCreate(e, 1);
         var zi = ZIndexComponent.getOrCreate(e);
         zi.value = zindex;
         var ph = new DepthPh(AVConstructor.create(DepthAxis, new AxisState(new Position(), new FractionSize(1))));
         ph.axisStates[depth].addSibling(new DepthAxisApplier(dc));
         e.addComponent(ph);
-        var c = new DepthRangeContainer(ph, 1);
-        c.setLayout(depth, PortionLayout.instance);
-        c.refreshOnChildrenChanged = true;
-        e.addComponent(c);
-        e.addComponent(new DepthBinder(c));
-        new CtxWatcher(DepthBinder, e, true);
 
-        ec.DebugInit.initCheck.listen((_:Entity) -> {
-            trace(e.getPath(), c.getChildren().map(ph -> ph.entity.name + " " + ph.axisStates[depth].getPos() + " " + ph.axisStates[depth].getSize()),
-                '[${dc.value}, ${dc.maxValue}]');
-        });
+        new CtxWatcher(DepthBinder, e, true);
+        return ph;
     }
 }
 
